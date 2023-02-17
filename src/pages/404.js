30fbdacb9cf44ee78aa7be2270e08b9bb4bbfba0
Pascal Betting <pascal.betting@gmx.de>
2023-02-17 11:40:34 +0100

    feat: add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,22 @@
+import { Link } from "gatsby";
 import * as React from "react";
 import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <main className="h-[calc(100vh-96px-244px)] bg-primary flex flex-col gap-8 justify-center items-center">
+      <h1 className="text-9xl text-onPrimaryHigh">404</h1>
+      <p className="text-xl text-onPrimaryMedium text-center">
+        Ups, sorry, da bist du leider an der falschen Stelle rausgekommen.
+        <br /> Hier geht es nicht weiter!
+      </p>
+      <Link
+        className="p-4 bg-orange-600 rounded-lg bg-accent text-onAccentHigh"
+        to="/"
+      >
+        Zurück zur Startseite
+      </Link>
+    </main>
   </Layout>
 );
 
